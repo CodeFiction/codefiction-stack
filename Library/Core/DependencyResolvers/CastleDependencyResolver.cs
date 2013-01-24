2a30a3f43ea231b8ac448c491763a11d50f51ff6
Mert Susur <mail@mertsusur.com>
2013-01-25 01:02:38 +0200

    Castle Dependency Resolver implemented.

--- Library/Core/DependencyResolvers/CastleDependencyResolver.cs
+++ Library/Core/DependencyResolvers/CastleDependencyResolver.cs
@@ -1,8 +1,68 @@
-﻿using CFCommerce.Library.CoreContracts;
+﻿using System;
+using System.Reflection;
+using CFCommerce.Library.CoreContracts;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using IDependencyResolver = CFCommerce.Library.CoreContracts.IDependencyResolver;
 
 namespace CfCommerce.Library.Core.DependencyResolvers
 {
     internal class CastleDependencyResolver : IDependencyResolver
     {
+        private readonly IWindsorContainer _container = new WindsorContainer();
+
+        public IDependencyResolver RegisterAssembly(Assembly assembly)
+        {
+            _container.Register(AllTypes.FromAssembly(assembly));
+            return this;
+        }
+
+        public IDependencyResolver RegisterInstance<TInterface>(TInterface instance)
+            where TInterface : class
+        {
+            return RegisterInstance(typeof(TInterface), instance);
+        }
+
+        public IDependencyResolver RegisterInstance(Type type, object instance)
+        {
+            _container.Register(Component.For(type).Instance(instance));
+            return this;
+        }
+
+        public IDependencyResolver Register<TInterface, TService>(InstanceMode mode = InstanceMode.Transient)
+            where TService : TInterface
+        {
+            return Register(typeof(TInterface), typeof(TService), mode);
+        }
+
+        public IDependencyResolver Register(Type interfaceType, Type serviceType, InstanceMode mode = InstanceMode.Transient)
+        {
+            ComponentRegistration<object> componentRegistration = Component.For(interfaceType).
+                ImplementedBy(serviceType);
+            if (mode == InstanceMode.Transient)
+            {
+                componentRegistration = componentRegistration.LifestyleTransient();
+            }
+            _container.Register(componentRegistration);
+            return this;
+        }
+
+        public TInterface Resolve<TInterface>()
+        {
+            return _container.Resolve<TInterface>();
+        }
+
+        public TInterface Resolve<TInterface>(string name)
+        {
+            return _container.Resolve<TInterface>(name);
+        }
+
+        public IDependencyResolver TearDown(object instance)
+        {
+            _container.Release(instance);
+            return this;
+        }
     }
 }
--- Library/CoreContracts/IDependencyResolver.cs
+++ Library/CoreContracts/IDependencyResolver.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace CFCommerce.Library.CoreContracts
 {
+    public enum InstanceMode { Singleton, Transient }
     public interface IDependencyResolver
     {
+        IDependencyResolver RegisterAssembly(Assembly assembly);
+
+        IDependencyResolver RegisterInstance<TInterface>(TInterface instance)
+            where TInterface : class;
+
+        IDependencyResolver RegisterInstance(Type type, object instance);
+
+        IDependencyResolver Register<TInterface, TService>(InstanceMode mode = InstanceMode.Transient) where TService : TInterface;
+
+        IDependencyResolver Register(Type interfaceType, Type serviceType, InstanceMode mode = InstanceMode.Transient);
+
+        TInterface Resolve<TInterface>();
+
+        TInterface Resolve<TInterface>(string name);
+
+        IDependencyResolver TearDown(object instance);
     }
 }
